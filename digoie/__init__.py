ad2586aff8dabe1e078f3756642cfe437711ed38
ZwEin <zwein27@gmail.com>
2016-02-05 20:07:24 -0800

    test setup

--- digoie/__init__.py
+++ digoie/__init__.py
@@ -1,22 +1,6 @@
 
-"""Set Program Entrance
-
-config interactive command in base
-"""
-# from digoie.core.commands import base
-
-
-# def run_from_terminal():
-#     # base.get_command()
-#     print 'terminal here'
-
-
-# if __name__ == '__main__':
-#     test()
-
-
-
+def main():
+    from digoie.core.commands import base
 
 if __name__ == '__main__':
-    from digoie.core.commands import base
-    pass
+    main()
--- setup.py
+++ setup.py
@@ -4,12 +4,16 @@
 # http://www.360doc.com/content/14/0306/11/13084517_358166737.shtml
 # http://python-packaging-user-guide.readthedocs.org/en/latest/distributing/
 # https://docs.python.org/2/distutils/setupscript.html
+# https://wiki.python.org/moin/TestPyPI
 
 
 """Setup
 
 sudo python setup.py install --record files.txt
 cat files.txt | sudo xargs rm -rf
+
+pip install -i https://testpypi.python.org/pypi digoie
+
 """
 
 
@@ -23,8 +27,13 @@ entrypoints['console_scripts'] = [
 
 setup(
     name = 'digoie',
-    version = '0.0.3',
+    version = '0.1.0',
+    description = 'A Open Information Extraction Library',
+    author = 'Peter Downs',
+    author_email = 'zwein27@gmail.com',
+    url = 'https://github.com/Airein/DIG-OpenIE',
     packages = find_packages(),
     entry_points = entrypoints,
-    package_data={'digoie': ['res/*']}
+    package_data={'digoie': ['res/*']},
+    install_requires=['scikit-learn']
     )
